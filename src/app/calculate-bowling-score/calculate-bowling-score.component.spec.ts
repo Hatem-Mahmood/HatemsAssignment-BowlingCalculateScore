aefcf27728476d38dcd8697f078c23d3a7e47540
Hatem-Mahmood <33034067+Hatem-Mahmood@users.noreply.github.com>
2018-08-26 18:09:57 +0200

    Add unit test
    Add the unit test to test all functions working without errors and test some probabilities

--- src/app/calculate-bowling-score/calculate-bowling-score.component.spec.ts
+++ src/app/calculate-bowling-score/calculate-bowling-score.component.spec.ts
@@ -22,4 +22,92 @@ describe('CalculateBowlingScoreComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+
+  it('can roll gutter game',()=>{
+    rollMany(0,20);
+      expect(component.score).toBe(0);
+   });
+   
+   it('can roll all once',()=>{
+      rollMany(1,20);
+      expect(component.score).toBe(20);
+   });
+   
+   it('can roll a strike',()=>{
+      component.roll(10);
+      component.roll(4);
+      component.roll(3);
+      rollMany(0,17);
+      expect(component.score).toBe(24);
+   });
+   
+   it('can roll a spare',()=>{
+      component.roll(5);
+      component.roll(5);
+      component.roll(2);
+      component.roll(3);
+      rollMany(0,16);
+      expect(component.score).toBe(17);
+   });
+   
+   it('can roll perfect game',()=>{
+      rollMany(10,12);
+      expect(component.score).toBe(300);
+   });
+   
+   it('can this do test 1',()=>{
+      component.roll(2); component.roll(8);
+      component.roll(3); component.roll(7);
+      component.roll(4); component.roll(6);
+      component.roll(2); component.roll(5);
+      rollMany(0,12);
+      expect(component.score).toBe(46);
+   });
+   
+   it('can this do test 2',()=>{
+      component.roll(1); component.roll(4);
+      component.roll(2); component.roll(4);
+      component.roll(1); component.roll(6);
+      component.roll(0); component.roll(8);
+      component.roll(3); component.roll(1);
+      component.roll(0); component.roll(8);
+      component.roll(5); component.roll(3);
+      component.roll(10); 
+      component.roll(4); component.roll(6);
+      component.roll(1); component.roll(6);
+      expect(component.score).toBe(84);
+   });
+   
+   it('can this do test 3',()=>{
+      component.roll(7); component.roll(3);
+      component.roll(4); component.roll(6);
+      component.roll(1); component.roll(9);
+      component.roll(4); component.roll(6);
+      component.roll(5); component.roll(5);
+      component.roll(9); component.roll(1);
+      component.roll(2); component.roll(8);
+      component.roll(3); component.roll(7); 
+      component.roll(3); component.roll(7);
+      component.roll(4); component.roll(6);
+      component.roll(5);
+      expect(component.score).toBe(140);
+   });
+   
+   it('can this do test 4',()=>{
+      component.roll(10); 
+      component.roll(10);
+      component.roll(10); 
+      component.roll(10);
+      rollMany(0,12);
+      expect(component.score).toBe(90);
+   });
+   
+   function rollMany(pins,rolls){
+     for (let i = 0; i < rolls; i++) {
+       component.roll(pins);
+     }
+   }
+   
+
 });
